89e5b5273b642c21b339d6d81cb43e2d5eec6efe
zaconja <40099134+zaconja@users.noreply.github.com>
2026-02-13 10:51:58 +0100

    milp_optimizer.py

--- milp_optimizer.py
+++ milp_optimizer.py
@@ -370,8 +370,16 @@ class _MILPCore:
         self._prob += objective, "TotalCost"
     
     def _solve(self):
-        solver = pl.PULP_CBC_CMD(msg=False, gapRel=0.001, timeLimit=60)
-        self._prob.solve(solver)
+    solver = pl.HiGHS_CMD(
+        msg=False, 
+        gapRel=0.001, 
+        timeLimit=60,
+        options=[
+            ("parallel", "on"),
+            ("solver", "ipm")   # "ipm" = interior point, "simplex" = simplex
+        ]
+    )
+    self._prob.solve(solver)
     
     def _extract_results(self) -> Dict[str, Any]:
         T = self.T
