bcd54f0a7daa1ca4653982002fbd0e5260c2fb59
zaconja <40099134+zaconja@users.noreply.github.com>
2026-02-13 13:10:24 +0100

    milp_optimizer.py

--- milp_optimizer.py
+++ milp_optimizer.py
@@ -1,47 +1,35 @@
-import numpy as np
 import pulp as pl
-from typing import List, Dict, Optional, Any
+import numpy as np
 
 class MILPDayAheadOptimizer:
-    def __init__(self,
-                 load,
-                 fne,
-                 spot_price,
-                 contracted_volume,
-                 contracted_price,
-                 batt_capacity_mwh,
-                 batt_power_mw,
+    def __init__(self, load, fne, spot_price,
+                 contracted_volume, contracted_price,
+                 batt_capacity_mwh, batt_power_mw,
                  batt_efficiency=0.9,
-                 co2_intensity=0.4,
-                 co2_price=80,
+                 co2_intensity=0.4, co2_price=80,
                  feedin_tariff=50,
                  batt_min_power=0.1,
                  batt_cycle_cost=5,
                  batt_startup_cost=0):
-
-        self.load = np.array(load).flatten()
-        self.fne = np.array(fne).flatten()
-        self.spot_price = np.array(spot_price).flatten()
-
-        self.contracted_volume = contracted_volume
-        self.contracted_price = contracted_price
-
+        self.T = 24
+        self.load = np.array(load)
+        self.fne = np.array(fne)
+        self.spot_price = np.array(spot_price)
+        self.contr_vol = contracted_volume
+        self.contr_price = contracted_price
         self.batt_cap = batt_capacity_mwh
         self.batt_pow = batt_power_mw
-        self.batt_eff = batt_efficiency
+        self.eff = batt_efficiency
+        self.co2_intensity = co2_intensity
+        self.co2_price = co2_price
+        self.feedin = feedin_tariff
         self.batt_min_power = batt_min_power
         self.batt_cycle_cost = batt_cycle_cost
         self.batt_startup_cost = batt_startup_cost
 
-        self.co2_intensity = co2_intensity
-        self.co2_price = co2_price
-        self.feedin_tariff = feedin_tariff
-
-        self.T = 24
-
     def optimize(self, initial_soc=0.0):
         T = self.T
-        prob = pl.LpProblem("DayAheadMILP", pl.LpMinimize)
+        prob = pl.LpProblem("DayAheadOptimization", pl.LpMinimize)
 
         # Varijable
         spot = pl.LpVariable.dicts("spot", range(T), lowBound=0, cat='Continuous')
@@ -51,21 +39,25 @@ class MILPDayAheadOptimizer:
         dis = pl.LpVariable.dicts("dis", range(T), lowBound=0, cat='Continuous')
         soc = pl.LpVariable.dicts("soc", range(T), lowBound=0, upBound=self.batt_cap, cat='Continuous')
 
-        # Binarne varijable
+        # Binarne varijable za stanje baterije
         u_ch = pl.LpVariable.dicts("u_ch", range(T), cat='Binary')
         u_dis = pl.LpVariable.dicts("u_dis", range(T), cat='Binary')
 
         # Ograničenja
+        # 1. Bilanca
         for t in range(T):
             prob += (self.fne[t] + contr[t] + dis[t] + spot[t] ==
                      self.load[t] + ch[t] + sales[t]), f"balance_{t}"
 
-        prob += pl.lpSum(contr[t] for t in range(T)) <= self.contracted_volume, "total_contract"
+        # 2. Ukupna ugovorena količina
+        prob += pl.lpSum(contr[t] for t in range(T)) <= self.contr_vol, "total_contract"
 
+        # 3. Dinamika baterije – popravljeno: umjesto dijeljenja koristi množenje s 1/eff
         prob += soc[0] == initial_soc, "soc_initial"
         for t in range(T-1):
-            prob += (soc[t+1] == soc[t] + self.batt_eff * ch[t] - dis[t] / self.batt_eff), f"soc_dynamic_{t}"
+            prob += (soc[t+1] == soc[t] + self.eff * ch[t] - dis[t] * (1.0 / self.eff)), f"soc_dynamic_{t}"
 
+        # 4. Ograničenja baterije
         for t in range(T):
             prob += ch[t] <= self.batt_pow * u_ch[t], f"ch_max_{t}"
             prob += dis[t] <= self.batt_pow * u_dis[t], f"dis_max_{t}"
@@ -74,13 +66,15 @@ class MILPDayAheadOptimizer:
                 prob += dis[t] >= self.batt_min_power * u_dis[t], f"dis_min_{t}"
             prob += u_ch[t] + u_dis[t] <= 1, f"ch_dis_mutex_{t}"
 
+        # 5. Funkcija cilja
         spot_cost = pl.lpSum((self.spot_price[t] + self.co2_intensity * self.co2_price) * spot[t] for t in range(T))
-        contr_cost = pl.lpSum(self.contracted_price * contr[t] for t in range(T))
-        sales_revenue = pl.lpSum(self.feedin_tariff * sales[t] for t in range(T))
+        contr_cost = pl.lpSum(self.contr_price * contr[t] for t in range(T))
+        sales_revenue = pl.lpSum(self.feedin * sales[t] for t in range(T))
         degradation_cost = self.batt_cycle_cost * pl.lpSum(ch[t] + dis[t] for t in range(T))
 
         prob += spot_cost + contr_cost + degradation_cost - sales_revenue, "TotalCost"
 
+        # Rješavanje
         solver = pl.PULP_CBC_CMD(msg=False)
         prob.solve(solver)
 
